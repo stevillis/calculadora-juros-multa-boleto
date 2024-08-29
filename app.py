22932f4c7a3eaa52f931bef4cf4f21e12b1422fc
stevillis <stevillis@hotmail.com>
2024-08-29 00:55:13 -0400

    refact: improve inputs descriptions

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ input_valor_principal = st.number_input(
     "Valor Principal (R$)", min_value=0.0, format="%.2f"
 )
 input_taxa_juros = st.number_input("Taxa de Juros (%)", min_value=0.0, format="%.2f")
-input_tipo_calculo = st.selectbox("Tipo de Cálculo", ["ao mês (a.m.)", "ao dia (a.d.)"])
+input_tipo_calculo = st.selectbox("", ["ao mês (a.m.)", "ao dia (a.d.)"])
 input_multa = st.number_input("Multa (%)", min_value=0.0, format="%.2f")
-input_dias_corridos = st.number_input("Dias Corridos", min_value=0)
+input_dias_corridos = st.number_input("Dias Corridos (desde o vencimento)", min_value=0)
 
 if input_tipo_calculo == "ao mês (a.m.)":
     taxa_juros_calculo = input_taxa_juros / 100
